a2d69d8bb416bbd325c4de306a3273e71a490339
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-27 22:52:15 +0100

    i hope it works

--- src/main/java/org/myprojectjava/MyCalculatrice.java
+++ src/main/java/org/myprojectjava/MyCalculatrice.java
@@ -3,7 +3,7 @@ package org.myprojectjava;
 public class MyCalculatrice {
 
     public void plusOperation(int a, int b){
-        System.out.println(a+b-7);
+        System.out.println(a+b);
     }
 
 }
