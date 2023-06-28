152cb326f14579fd501cb14c813a62661ed55b64
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-28 15:54:22 +0100

    cafor

--- project/src/main/java/org/myprojectjava/MyCalculatrice.java
+++ project/src/main/java/org/myprojectjava/MyCalculatrice.java
@@ -3,7 +3,7 @@ package org.myprojectjava;
 public class MyCalculatrice {
 
     public void plusOperation(int a, int b){
-        System.out.println("pls 3 3 3 3 3");
+        System.out.println("pls 3 3 3 3 3 marjane");
         System.out.println(a+b+3);
     }
 
