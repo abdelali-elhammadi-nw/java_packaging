546457b0948ea20ea8508c30d71fb4f465eaccca
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-28 16:56:18 +0100

    aaa

--- project/src/main/java/org/myprojectjava/MyCalculatrice.java
+++ project/src/main/java/org/myprojectjava/MyCalculatrice.java
@@ -3,8 +3,8 @@ package org.myprojectjava;
 public class MyCalculatrice {
 
     public void plusOperation(int a, int b){
-        System.out.println("pls 3 3 333");
         System.out.println(a+b+3);
+        System.out.println("pls 3 3 333");
     }
 
 }
