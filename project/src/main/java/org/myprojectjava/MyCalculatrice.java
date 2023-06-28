1877cd2b4805678818f27b610d2ac4be14f6407b
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-28 16:28:59 +0100

    paramedi

--- project/src/main/java/org/myprojectjava/MyCalculatrice.java
+++ project/src/main/java/org/myprojectjava/MyCalculatrice.java
@@ -3,7 +3,7 @@ package org.myprojectjava;
 public class MyCalculatrice {
 
     public void plusOperation(int a, int b){
-        System.out.println("pls 3 3 s");
+        System.out.println("pls 3 3 333");
         System.out.println(a+b+3);
     }
 
