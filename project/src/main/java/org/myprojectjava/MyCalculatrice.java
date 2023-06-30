b0273b6a66519e4e66bf6b6c3d2d2dec89d85a04
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-30 22:36:15 +0100

    jhaha

--- project/src/main/java/org/myprojectjava/MyCalculatrice.java
+++ project/src/main/java/org/myprojectjava/MyCalculatrice.java
@@ -4,7 +4,7 @@ public class MyCalculatrice {
 
     public void plusOperation(int a, int b){
         System.out.println(a+b+3);
-        System.out.println("pls 3 3 333 apah");
+        System.out.println("best");
         System.out.println("i zzz");
     }
 
